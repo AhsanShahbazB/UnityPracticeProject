8804b8aad6530e8d701a9c9ac8b6799a49060211
AhsanShahbazB <ahsanshahbazbutt6@gmail.com>
2023-09-08 15:42:11 +0500

    wrote logic for instantiating cards on screen to match

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,8 +12,16 @@ public class GameManager : MonoBehaviour
         _2x3,_3x2
     };
 
+    private int _totalCards;
 
     public Layouts SelectedUserLayout;
+
+    public ScrollRect gameplayCardsScrollView;
+
+    public GameObject CardPrefab;
+
+    public GameObject LayoutPanel;
+    public GameObject GamePanel;
 
     private void Awake()
     {
@@ -33,11 +41,28 @@ public class GameManager : MonoBehaviour
         if(selectLayout == 0)
         {
             SelectedUserLayout = Layouts._2x3;
+            gameplayCardsScrollView.content.GetComponent<GridLayoutGroup>().constraintCount = 3;
+            _totalCards = 2 * 3;
         }
         else
         {
             SelectedUserLayout = Layouts._3x2;
+            gameplayCardsScrollView.content.GetComponent<GridLayoutGroup>().constraintCount = 2;
+            _totalCards = 3 * 2;
         }
+        GamePanel.SetActive(true);
+        LayoutPanel.SetActive(false);
+        StartCoroutine(StartGame());
     }
 
+    public IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for(int i = 0; i < _totalCards; i++)
+        {
+            Instantiate(CardPrefab, gameplayCardsScrollView.content);
+            yield return new WaitForSeconds(0.5f);
+        }
+        yield return null;
+    }
 }
